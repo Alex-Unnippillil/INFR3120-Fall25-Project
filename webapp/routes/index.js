49627e1120c9b7ae74391f5222455d601c2e827a
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-11-27 03:32:25 +0000

    Protect schedule - auth guard - when not logged in redirect to login page

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,24 +1,31 @@
 var express = require('express');
 var router = express.Router();
-const { ensureLoggedIn } = require('../middleware/auth'); // ADD THIS
+
+//auth guard // when not logged in redirect to /login page
+function requireLogin(req, res, next) {
+  if (!req.session || !req.session.user) {
+    return res.redirect('/login');
+  }
+  next();
+}
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index', { title: 'Home' });
 });
 
 /* GET /home (same as /). */
-router.get('/home', function(req, res) {
+router.get('/home', function (req, res) {
   res.render('index', { title: 'Home' });
 });
 
-/* GET schedule page (protected) */
-router.get('/schedule', ensureLoggedIn, function(req, res) {
+/* GET schedule page (protected). */
+router.get('/schedule', requireLogin, function (req, res) {
   res.render('schedule', { title: 'Schedule' });
 });
 
 /* GET help page. */
-router.get('/help', function(req, res) {
+router.get('/help', function (req, res) {
   res.render('help', { title: 'Info' });
 });
 
