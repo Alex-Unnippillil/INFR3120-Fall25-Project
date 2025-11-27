79ab4662caa18748f96194779a888b8ca5418d3d
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-11-27 03:10:14 +0000

    mongoDB initalization + set up sessions in app.js

--- webapp/app.js
+++ webapp/app.js
@@ -8,19 +8,52 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const { title } = require('process');
 
+var session = require('express-session'); // session middleware
+var mongoose = require('mongoose'); // mongoose for MongoDB
+
 var app = express();
+
+// MongoDB connection
+const mongoUri = process.env.MONGODB_URI;
+
+if (!mongoUri) {
+  console.warn('MONGODB_URI is not set yet warning');
+} else {
+  mongoose
+    .connect(mongoUri)
+    .then(() => {
+      console.log('Successfully connected to MongoDB');
+    })
+    .catch((err) => {
+      console.error('MongoDB has a connection error:', err);
+    });
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Session middleware
+const sessionSecret = process.env.SESSION_SECRET || 'dev-secret-change-me';
+
+app.use(
+  session({
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 // 1 day , secure true in production with HTTPS
+    }
+  })
+);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
-
 
 // make `user` available in all ejs views
 app.use((req, res, next) => {
