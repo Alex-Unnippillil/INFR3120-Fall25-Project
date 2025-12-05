ac6da3626a496568305622aef52b268e62a1e7a3
Mathew Arogbo <mathew.arogbo@ontariotechu.net>
2025-12-04 23:29:40 -0500

    Pass user session into views and protect schedule

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+<<<<<<< HEAD
 
 //auth guard // when not logged in redirect to /login page
 function requireLogin(req, res, next) {
@@ -27,6 +28,40 @@ router.get('/schedule', requireLogin, function (req, res) {
 /* GET help page. */
 router.get('/help', function (req, res) {
   res.render('help', { title: 'Info' });
+=======
+const { ensureLoggedIn } = require('../middleware/auth');
+
+// GET home page.
+router.get('/', function(req, res) {
+  res.render('index', { 
+    title: 'Home',
+    user: req.session.user
+  });
+});
+
+// GET /home (same as /).
+router.get('/home', function(req, res) {
+  res.render('index', { 
+    title: 'Home',
+    user: req.session.user
+  });
+});
+
+// GET schedule page (protected)
+router.get('/schedule', ensureLoggedIn, function(req, res) {
+  res.render('schedule', { 
+    title: 'Schedule',
+    user: req.session.user
+  });
+});
+
+// GET help page.
+router.get('/help', function(req, res) {
+  res.render('help', { 
+    title: 'Info',
+    user: req.session.user
+  });
+>>>>>>> 367ebbf (Pass user session into views and protect schedule)
 });
 
 module.exports = router;
