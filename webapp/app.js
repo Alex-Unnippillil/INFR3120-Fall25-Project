52507a12fb334134c2ee7e822a54cdc61bde4534
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-12-05 16:19:18 +0000

    Refactor error logging and improve comments for clarity in app.js

--- webapp/app.js
+++ webapp/app.js
@@ -25,19 +25,20 @@ if (!mongoUri) {
       console.log('Successfully connected to MongoDB');
     })
     .catch((err) => {
-      console.error('MongoDB has a connection error:', err);
+      console.error('MongoDB connection error:', err);
     });
 }
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//Session middleware
+// Session middleware
 const sessionSecret = process.env.SESSION_SECRET || 'dev-secret-change-me';
 
 app.use(
@@ -47,31 +48,32 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 // 1 day , secure true in production with HTTPS
+      maxAge: 1000 * 60 * 60 * 24 // 1 day; set secure: true in production with HTTPS
     }
   })
 );
 
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-// make `user` available in all ejs views
+// Make `user` available in all EJS views
 app.use((req, res, next) => {
   res.locals.user = req.session?.user || null;
   next();
 });
 
-
+// Routers
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
 app.use('/', usersRouter);
 
+// catch 404 and forward TO THEerror handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-app.use(function(err, req, res, next) {
-
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
